feb47f70b2cd468a0bafceb32a2ec86233de85ea
Ethan-Genser <ethanp1033@gmail.com>
2019-04-08 19:20:13 +0000

    removed STATICFILES_DIR

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/assets/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'assets'),
+#]
